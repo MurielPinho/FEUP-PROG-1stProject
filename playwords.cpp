ba1867d34f587b93b355f5cd3fc60bd63d995767
MurielPinho <muriel.apinho@gmail.com>
2018-04-10 16:57:01 +0100

    regex consertado

--- playwords.cpp
+++ playwords.cpp
@@ -11,7 +11,7 @@
 using namespace std;
 using namespace std::regex_constants;
 
-void strupper(string &s);
+void strupper(string s);
 int playwords(string pattern);
 bool checkWord(const string word, const vector<string> lista);
 string guessWord(const vector<string> lista, const int num);
@@ -19,102 +19,102 @@ void searchWord(string letters, const vector<string> lista);
 int main()
 {
 
-    int op, a, n, i = 3;
-    vector<string> dic;
-    string word, file4read, line, aux, aux2;
-    ifstream infile;
+        int op, a, n, i = 3;
+        vector<string> dic;
+        string word, file4read, line, aux, aux2;
+        ifstream infile;
 
 
         srand((unsigned int)time(0));
 //Leitura do arquivo
 
-    cout << "Input file name: " << endl;
-    getline(cin, file4read);
-    infile.open(file4read);
-    if (infile.fail())
-	{
-		cerr << "Error opening file: " << file4read << endl;
-		exit(1);
-	}
+        cout << "Input file name: " << endl;
+        getline(cin, file4read);
+        infile.open(file4read);
+        if (infile.fail())
+        {
+                cerr << "Error opening file: " << file4read << endl;
+                exit(1);
+        }
 //Transferencia para o vetor
-    while(!infile.eof())
-	{
-        getline(infile, line);
-        dic.push_back(line);
-    }
-    infile.close();
+        while(!infile.eof())
+        {
+                getline(infile, line);
+                dic.push_back(line);
+        }
+        infile.close();
 
-    cout << "PLAYWORDS" << endl;
-    cout << "=======================================" << endl;
-    cout << "Choose your option: " << endl;
-    cout << "Option 1: Check a word" << endl;
-    cout << "Option 2: Guess a word" << endl;
-    cout << "Option 3: Search a word by letters" << endl;
-    cout << "Option 4: Build a word" << endl;
-    cout << "Option 5: Wildcards!" << endl;
+        cout << "PLAYWORDS" << endl;
+        cout << "=======================================" << endl;
+        cout << "Choose your option: " << endl;
+        cout << "Option 1: Check a word" << endl;
+        cout << "Option 2: Guess a word" << endl;
+        cout << "Option 3: Search a word by letters" << endl;
+        cout << "Option 4: Build a word" << endl;
+        cout << "Option 5: Wildcards!" << endl;
 
-    do
-    {
-        cin >> op;
-        if(op>5 || op<1)
-            cout << "Please enter a vaid option (between 1 and 5)" << endl;
-    } while(op>5 || op<1);
-//limpando o buffer
-    cin.clear();
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-    switch (op)
-    {
-    case 1:
-        getline(cin, word);
-        strupper(word);
-        if(checkWord(word, dic))
-            cout << "Word exists!" << endl;
-        else
-            cout << "Word does not exists" << endl;
-        break;
-    case 2:
-        n = rand () % dic.size();
-        aux = guessWord(dic, n);
-        aux2 = dic.at(n);
-        cout << "Form a word with: "<< aux << endl;
         do
         {
-            getline(cin, word);
-            if(aux2 != word)
-            {
-                i--;
-                cout << "Try Again(" << i << " chances left!)" << endl;
-            }
-            else
+                cin >> op;
+                if(op>5 || op<1)
+                        cout << "Please enter a vaid option (between 1 and 5)" << endl;
+        } while(op>5 || op<1);
+//limpando o buffer
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        switch (op)
+        {
+        case 1:
+                getline(cin, word);
+                strupper(word);
+                if(checkWord(word, dic))
+                        cout << "Word exists!" << endl;
+                else
+                        cout << "Word does not exists" << endl;
                 break;
-        }while(i > 0);
+        case 2:
+                n = rand () % dic.size();
+                aux = guessWord(dic, n);
+                aux2 = dic.at(n);
+                cout << "Form a word with: "<< aux << endl;
+                do
+                {
+                        getline(cin, word);
+                        if(aux2 != word)
+                        {
+                                i--;
+                                cout << "Try Again(" << i << " chances left!)" << endl;
+                        }
+                        else
+                                break;
+                } while(i > 0);
 
-        if(aux2 == word)
-            cout << "Winner Winner, Chicken Dinner!!!" << endl;
-        else
-            cout << "Better luck next time, Loser!" << endl;
-        break;
-    case 3:
-        cout << "Provide the set of letters: ";
-        getline(cin, word);
-        searchWord(word, dic);
-        break;
-    // case 4:
-    //     break;
-    case 5:
-        cout << "Insert Wildcard: ";
-        //getline(cin, word);
-        if((a = (playwords(word)) < 0))
-            cout << "Error opening file" << endl;
-        else if(a==0)
-            cout << "No matching words" << endl;
-        break;
-    default:
-        break;
-    }
+                if(aux2 == word)
+                        cout << "Winner Winner, Chicken Dinner!!!" << endl;
+                else
+                        cout << "Better luck next time, Loser!" << endl;
+                break;
+        case 3:
+                cout << "Provide the set of letters: ";
+                getline(cin, word);
+                searchWord(word, dic);
+                break;
+        // case 4:
+        //     break;
+        case 5:
+                cout << "Insert Wildcard: ";
+                //getline(cin, word);
+                if((a = (playwords(word)) < 0))
+                        cout << "Error opening file" << endl;
+                else if(a==0)
+                        cout << "No matching words" << endl;
+                break;
+        default:
+                break;
+        }
 
-    return 0;
+        return 0;
 
 }
 //Funcoes
@@ -124,7 +124,7 @@ int playwords(string pattern)
                line;
         regex reg;
         ifstream file;
-        int flag;
+        int flag = 0;
         for (size_t i = 0; i < new_pattern.length(); i++)
         {
                 if (new_pattern.at(i) == '?')
@@ -169,12 +169,12 @@ bool checkWord(const string word, const vector<string> lista)
 string guessWord(const vector<string> lista, const int num)
 {
 
-    string word;
-    int seed;
+        string word;
+        int seed;
 
-    seed = chrono::system_clock::now().time_since_epoch().count();
+        seed = chrono::system_clock::now().time_since_epoch().count();
 
-    word = lista.at(num);
+        word = lista.at(num);
 
 
         shuffle(word.begin(), word.end(), default_random_engine(seed));
@@ -184,35 +184,36 @@ string guessWord(const vector<string> lista, const int num)
 
 void searchWord(string letters, const vector<string> lista)
 {
-    int flag;
+        int flag;
 
-    for (size_t i = 0; i < lista.size(); i++)
-    {
-        if (lista.at(i).size() >= letters.size())
+        for (size_t i = 0; i < lista.size(); i++)
         {
-            for (size_t j = 0; j < lista.at(i).size(); j++) //verifica letras da palavra do vetor
-            {
-                for (size_t k = 0; k < letters.size(); k++) //verifica letras fornecidas
+                if (lista.at(i).size() >= letters.size())
                 {
-                    if(letters.at(k)==lista.at(i).at(j))
-                    {
-                        flag = 1;
-                        k = letters.size();
-                    }
-                    else
-                        flag = 0;
+                        for (size_t j = 0; j < lista.at(i).size(); j++) //verifica letras da palavra do vetor
+                        {
+                                for (size_t k = 0; k < letters.size(); k++) //verifica letras fornecidas
+                                {
+                                        if(letters.at(k)==lista.at(i).at(j))
+                                        {
+                                                flag = 1;
+                                                k = letters.size();
+                                        }
+                                        else
+                                                flag = 0;
+                                }
+                        }
                 }
-            }
+                if(flag==1)
+                        cout << lista.at(i) << endl;
         }
-        if(flag==1)
-            cout << lista.at(i) << endl;
-    }
 }
 
-void strupper(string &s)
+void strupper(string s)
 {
-    for(size_t i; i < s.size(); i++)
-    {
-        isupper(s.at(i));
-    }
+        for(size_t i; i < s.size(); i++)
+        {
+                isupper(s.at(i));
+                cout << s.at(i);
+        }
 }
